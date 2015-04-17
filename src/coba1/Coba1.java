39ed5cbbf821c93764c329cb3ebeb29a627da173
SBC4 <SBC4@SBC13>
2015-04-17 17:22:41 +0700

    heri purnomo

---
+++ src/coba1/Coba1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coba1;
+
+/**
+ *
+ * @author SBC4
+ */
+public class Coba1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
